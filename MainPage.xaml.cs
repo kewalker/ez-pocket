8d90bb33d8f6354afafee14c3026c3ed4746247a
Kenneth Walker <kewalker@uvic.ca>
2026-09-12 19:08:49 -0700

    Add pointer hover feedback to Scan

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -12,6 +12,35 @@ public partial class MainPage : ContentPage
         scanner = IPlatformApplication.Current?.Services.GetService<PocketScanner>() ?? new PocketScanner();
     }
 
+    private void OnScanPointerEntered(object? sender, PointerEventArgs e)
+    {
+        ScanButton.Scale = 1.02;
+        ScanButton.Opacity = 0.9;
+    }
+
+    private void OnScanPointerExited(object? sender, PointerEventArgs e)
+    {
+        ResetScanButton();
+    }
+
+    private void OnScanPressed(object? sender, EventArgs e)
+    {
+        ScanButton.Scale = 0.96;
+        ScanButton.Opacity = 0.75;
+    }
+
+    private void OnScanReleased(object? sender, EventArgs e)
+    {
+        ScanButton.Scale = 1.02;
+        ScanButton.Opacity = 0.9;
+    }
+
+    private void ResetScanButton()
+    {
+        ScanButton.Scale = 1;
+        ScanButton.Opacity = 1;
+    }
+
     private async void OnScanClicked(object? sender, EventArgs e)
     {
         var drives = scanner.Scan();
